0ee9a16d95d025d26cc372e01384fa75bfdb8e8f
andy_zhaozhao@hotmail.com <andy_zhaozhao@hotmail.com>
2019-08-07 12:13:58 +0800

    Mod: cors实验前端

--- securityday5/src/main/java/cn/com/taiji/security/securityday5/web/DemoController.java
+++ securityday5/src/main/java/cn/com/taiji/security/securityday5/web/DemoController.java
@@ -2,12 +2,16 @@ package cn.com.taiji.security.securityday5.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class DemoController {
     private Logger logger = LoggerFactory.getLogger(DemoController.class);
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @GetMapping("/admin")
     public String admin() {
@@ -19,13 +23,28 @@ public class DemoController {
         return "return user";
     }
 
+    @DeleteMapping("/user")
+    public String d() {
+        return "删除用户成功";
+    }
+
+    @PutMapping("/user")
+    public String update() {
+        return "更新用户成功";
+    }
+
+    @PostMapping("/user")
+    public String add() {
+        return "增加用户成功";
+    }
+
     @GetMapping("/a")
     public String a() {
         return "return a";
     }
-
     @GetMapping("/b")
-    public String b() {
-        return "return b";
+    public Object b() {
+        Object result = restTemplate.getForObject("https://www.baidu.com/",String.class);
+        return result;
     }
 }
